aca91daf2dd7a84c66c4e216b4a0e34bffbaf400
Diassvinicius <vghs004@gmail.com>
2025-05-15 14:42:34 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,41 +2,85 @@
 
 int main (){
 
-char estado;
-char codigo;
-char nome [20];
-float populacao;
-float area;
-float pib;
-int turistas;
+char estado1[1], estado2[1];
+char codigo1[4], codigo2[4];
+char nome1[50], nome2[50];
+int populacao1, populacao2;
+float area1, area2;
+float pib1, pib2;
+int turismo1, turismo2;
 
-printf("Carta 1: \n");
+// Aqui começa a inscrição da 1 carta
+
+printf("_BEM VINDO AO SUPER TRUNFO_\n\n");
 
 printf("Digite o Estado: \n");
-scanf("%c", &estado);
+scanf("%c", &estado1);
 
 printf("Digite o codigo da carta: \n");
-scanf("%s", &codigo);
+scanf("%s", codigo1);
 
 printf("Digite o nome da cidade: \n");
-scanf("%s", nome);
+scanf("%s", nome1);
 
 printf("Digite a população: \n");
-scanf("%e", &populacao);
+scanf("%d", &populacao1);
 
 printf("Digite a area: \n");
-scanf("%e", &area);
+scanf("%f", &area1);
 
 printf("Digite o PIB: \n");
-scanf("%e", &pib);
+scanf("%e", &pib1);
 
-printf("Digite a quantidade de turistas: \n");
-scanf("%d", &turistas);
+printf("Digite a quantidade de pontos turisticos: \n");
+scanf("%d", &turismo1);
 
-printf("Código da Carta: %d\n", codigo);
-printf("Nome: %s\n", nome);
-printf("População: %f Milhões\n", populacao);
-printf("Area Territorial %f Km\n", area);
-printf("PIB %f bilhões\n", pib);
-printf("Quantidade de Turistas %d mil", turistas);
+// Aqui começa a inscrição da 2 carta
+
+printf("Digite o Estado: \n");
+scanf("%c", &estado2);
+
+printf("Digite o codigo da carta: \n");
+scanf("%s", codigo2);
+
+printf("Digite o nome da cidade: \n");
+scanf("%s", nome2);
+
+printf("Digite a população: \n");
+scanf("%d", &populacao2);
+
+printf("Digite a area: \n");
+scanf("%f", &area2);
+
+printf("Digite o PIB: \n");
+scanf("%f", &pib2);
+
+printf("Digite a quantidade de pontos turisticos: \n");
+scanf("%d", &turismo2);
+
+//Aqui começa a exibição de dados da carta 1
+
+printf("_DADOS DA CARTA 01_\n\n");
+
+printf("Estado %c\n", estado1);
+printf("Código da Carta: %d\n", codigo1);
+printf("Nome: %s\n", nome1);
+printf("População: %d Milhões\n", populacao1);
+printf("Area Territorial %.2f Km\n", area1);
+printf("PIB %.2f bilhões\n", pib1);
+printf("Quantidade de Pontos Turisticos %d\n", turismo1);
+
+//Aqui começa a exibição de dados da carta 2
+
+printf("_DADOS DA CARTA 02_\n\n");
+
+printf("Estado %c\n", estado2);
+printf("Código da Carta: %d\n", codigo2);
+printf("Nome: %s\n", nome2);
+printf("População: %d Milhões\n", populacao2);
+printf("Area Territorial %.2f Km\n", area2);
+printf("PIB %.2f bilhões\n", pib2);
+printf("Quantidade de Pontos Turisticos %d", turismo2);
+
+return 0;
 }
